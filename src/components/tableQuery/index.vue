bc63a61a3ff1810ae16303d1d7b3dcd22428d41a
古德猫宁 <2426104560@qq.com>
2024-04-30 11:47:28 +0800

    新增项目列表

---
+++ src/components/tableQuery/index.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { Refresh, Search } from '@element-plus/icons-vue'
+
+defineOptions({
+  name: 'TableQuery',
+})
+defineProps(['listLoading', 'fold'])
+const emit = defineEmits(['queryData', 'onReset', 'handleFold'])
+const queryData = () => emit('queryData')
+const onReset = () => emit('onReset')
+const handleFold = () => emit('handleFold')
+</script>
+
+<template>
+  <el-form-item>
+    <el-button :icon="Search" :loading="listLoading" native-type="submit" type="primary" @click="queryData">
+      查询
+    </el-button>
+    <el-button :icon="Refresh" :loading="listLoading" native-type="submit" @click="onReset">
+      重置
+    </el-button>
+    <el-button class="hidden-xs-only" text type="primary" @click="handleFold">
+      <span v-if="fold">展开</span>
+      <span v-else>合并</span>
+    </el-button>
+  </el-form-item>
+</template>
