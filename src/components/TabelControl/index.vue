14db40a7cdba5e34e71c7b5b3cefa1d6f3f9a828
llg <3102093662@qq.com>
2024-04-30 18:01:43 +0800

    用户管理-客户列表

---
+++ src/components/TabelControl/index.vue
@@ -0,0 +1,104 @@
+<!-- eslint-disable vue/valid-define-props -->
+<!-- eslint-disable ts/no-use-before-define -->
+<script setup>
+import { defineProps, toRefs } from 'vue'
+import VabDraggable from 'vuedraggable'
+
+defineOptions({
+  name: 'TableControl',
+})
+const props = defineProps({
+  border: Boolean,
+  columns: Array,
+  isFullscreen: Boolean,
+  lineHeight: String,
+  stripe: Boolean,
+  checkList: Array,
+})
+const emit = defineEmits(['clickFullScreen', 'queryData', 'update:stripe', 'update:border', 'update:lineHeight', 'update:checkList'])
+const { border, columns, isFullscreen, lineHeight, stripe, checkList } = toRefs(props)
+const clickFullScreen = () => emit('clickFullScreen')
+const queryData = () => emit('queryData')
+function changeStripe() {
+  emit('update:stripe', !stripe.value)
+}
+function changeBorder() {
+  emit('update:border', !border.value)
+}
+function changeRadio(a) {
+  emit('update:lineHeight', a)
+}
+function changeCheckbox(a) {
+  emit('update:checkList', a)
+
+  // emit('update:lineHeight', a)
+}
+const dragOptions = computed(() => {
+  return {
+    animation: 600,
+    group: 'description',
+  }
+})
+</script>
+
+<template>
+  <div>
+    <div class="custom-table-right-tools">
+      <el-button class="hidden-xs-only" @click="changeStripe">
+        <el-checkbox v-model="stripe" label="斑马纹" />
+      </el-button>
+      <el-button class="hidden-xs-only" @click="changeBorder">
+        <el-checkbox v-model="border" label="边框" />
+      </el-button>
+      <el-button @click="queryData">
+        <div class="i-flowbite:refresh-outline h-1.5em w-1.5em" />
+      </el-button>
+      <el-button @click="clickFullScreen">
+        <div class="i-material-symbols:fullscreen h-1.5em w-1.5em" />
+      </el-button>
+      <el-popover trigger="hover" :width="162">
+        <el-radio-group v-model="lineHeight" @change="changeRadio">
+          <el-radio-button label="large">
+            大
+          </el-radio-button>
+          <el-radio-button label="default">
+            中
+          </el-radio-button>
+          <el-radio-button label="small">
+            小
+          </el-radio-button>
+        </el-radio-group>
+        <template #reference>
+          <el-button>
+            <div class="i-mdi:sort h-1.5em w-1.5em" />
+          </el-button>
+        </template>
+      </el-popover>
+      <el-popover popper-class="custom-table-checkbox" trigger="hover">
+        <template #reference>
+          <el-button>
+            <div class="i-tabler:settings-2 h-1.5em w-1.5em" />
+          </el-button>
+        </template>
+        <el-checkbox-group v-model="checkList" @change="changeCheckbox">
+          <VabDraggable item-key="{ element }" :list="JSON.parse(JSON.stringify(columns))" v-bind="dragOptions">
+            <template #item="{ element }">
+              <div>
+                <el-checkbox :disabled="element.disableCheck" :label="element.prop">
+                  {{ element.label }}
+                </el-checkbox>
+              </div>
+            </template>
+          </VabDraggable>
+        </el-checkbox-group>
+      </el-popover>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.custom-table-right-tools {
+  display: flex;
+  align-items: center;
+}
+</style>
---
+++ src/views/user/customer/components/customerTopTabs.vue
@@ -0,0 +1,126 @@
+<script lang="ts" setup>
+import type { FormRules } from 'element-plus'
+import { ElForm } from 'element-plus'
+
+import { defineProps, inject, ref } from 'vue'
+// 如果希望默认展示第一个 Tab
+const props = defineProps({
+  leftTab: Object,
+  tabIndex: Number,
+})
+
+const emit = defineEmits(['setClient'])
+const isShow = ref(false)
+const isTrue = ref(true)
+function showEdit() {
+  isShow.value = true
+}
+defineExpose({ showEdit })
+
+// 使用 InstanceType 来获取 ElForm 实例的类型
+const formRef = ref(null)
+// 注入主组件中的提供者
+const localToptTab = ref<any>(props.leftTab)
+</script>
+
+<template>
+  <div>
+    <ElForm ref="formRef" label-width="100px" model="" rules="rules">
+      <el-card class="box-card">
+        <template #header>
+          <div class="card-header">
+            <span>基本信息</span>
+          </div>
+        </template>
+        <el-row :gutter="10">
+          <el-col :span="12">
+            <el-form-item label="客户名称" prop="name" style="float: left; width: 18.5rem;">
+              <el-input clearable />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="客户简称" prop="client_pid" style="float: left; width: 18.5rem;">
+              <el-input clearable />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="12">
+            <el-form-item label="公司名称" prop="name" style="float: left; width: 18.5rem;">
+              <el-input clearable />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="客户姓名" prop="client_pid" style="float: left; width: 18.5rem;">
+              <el-input clearable />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="10">
+          <el-col :span="12">
+            <el-form-item label="手机号码" prop="name" style="float: left; width: 18.5rem;">
+              <el-input clearable />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="电子邮箱" prop="client_pid" style="float: left; width: 18.5rem;">
+              <el-input clearable />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <el-form-item label="负责人" prop="money">
+              <el-select />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="结算周期" prop="quota">
+              <el-select />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-divider content-position="left">
+          权限信息
+        </el-divider>
+        <el-row :gutter="20">
+          <el-form-item label="风险控制" prop="top">
+            <el-switch v-model="isTrue" />
+          </el-form-item>
+        </el-row>
+        <div v-if="isTrue">
+          <el-row :gutter="20">
+            <el-form-item label="营业限额/月" prop="top">
+              <el-input clearable />
+            </el-form-item>
+          </el-row>
+          <el-row :gutter="20">
+            <el-form-item label="审核率Min值" prop="top">
+              <el-input clearable />
+            </el-form-item>
+          </el-row>
+        </div>
+
+        <el-row :gutter="20">
+          <el-form-item label="客户状态" prop="top">
+            <el-switch />
+          </el-form-item>
+        </el-row>
+        <el-divider content-position="left" />
+        <el-row :gutter="20">
+          <el-col :span="16" />
+          <el-col :span="8">
+            <el-form-item prop="platform">
+              <el-button size="default" @click="">
+                暂存
+              </el-button>
+              <el-button type="primary" size="default" @click="">
+                添加
+              </el-button>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-card>
+    </ElForm>
+  </div>
+</template>
