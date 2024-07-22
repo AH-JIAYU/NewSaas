5852a2bba1c941d9f8f67d22e80e454941693279
llg <3102093662@qq.com>
2024-07-22 12:50:24 +0800

    修改角色管理

--- src/store/modules/route.ts
+++ src/store/modules/route.ts
@@ -1,45 +1,60 @@
-import { cloneDeep } from 'lodash-es'
-import type { RouteMeta, RouteRecordRaw } from 'vue-router'
-import useSettingsStore from './settings'
-import useTabbarStore from './tabbar'
-import { resolveRoutePath } from '@/utils'
-import { systemRoutes } from '@/router/routes'
-import apiApp from '@/api/modules/app'
-import type { Route } from '#/global'
+import { cloneDeep } from "lodash-es";
+import type { RouteMeta, RouteRecordRaw } from "vue-router";
+import useSettingsStore from "./settings";
+import useTabbarStore from "./tabbar";
+import { resolveRoutePath } from "@/utils";
+import { systemRoutes } from "@/router/routes";
+import apiApp from "@/api/modules/app";
+import type { Route } from "#/global";
 
 const useRouteStore = defineStore(
   // 唯一ID
-  'route',
+  "route",
   () => {
-    const settingsStore = useSettingsStore()
-    const tabbarStore = useTabbarStore()
+    const settingsStore = useSettingsStore();
+    const tabbarStore = useTabbarStore();
 
-    const isGenerate = ref(false)
-    const routesRaw = ref<Route.recordMainRaw[]>([])
-    const filesystemRoutesRaw = ref<RouteRecordRaw[]>([])
-    const currentRemoveRoutes = ref<(() => void)[]>([])
+    const isGenerate = ref(false);
+    const routesRaw = ref<Route.recordMainRaw[]>([]);
+    const filesystemRoutesRaw = ref<RouteRecordRaw[]>([]);
+    const currentRemoveRoutes = ref<(() => void)[]>([]);
 
     // 将多层嵌套路由处理成两层，保留顶层和最子层路由，中间层级将被拍平
     function flatAsyncRoutes<T extends RouteRecordRaw>(route: T): T {
       if (route.children) {
-        route.children = flatAsyncRoutesRecursive(route.children, [{
-          path: route.path,
-          title: route.meta?.title,
-          i18n: route.meta?.i18n,
-          icon: route.meta?.icon,
-          activeIcon: route.meta?.activeIcon,
-          hide: !route.meta?.breadcrumb && route.meta?.breadcrumb === false,
-        }], route.path, route.meta?.auth)
+        route.children = flatAsyncRoutesRecursive(
+          route.children,
+          [
+            {
+              path: route.path,
+              title: route.meta?.title,
+              i18n: route.meta?.i18n,
+              icon: route.meta?.icon,
+              activeIcon: route.meta?.activeIcon,
+              hide: !route.meta?.breadcrumb && route.meta?.breadcrumb === false,
+            },
+          ],
+          route.path,
+          route.meta?.auth
+        );
       }
-      return route
+      return route;
     }
-    function flatAsyncRoutesRecursive(routes: RouteRecordRaw[], breadcrumb: Route.breadcrumb[] = [], baseUrl = '', baseAuth: RouteMeta['auth']): RouteRecordRaw[] {
-      const res: RouteRecordRaw[] = []
+    function flatAsyncRoutesRecursive(
+      routes: RouteRecordRaw[],
+      breadcrumb: Route.breadcrumb[] = [],
+      baseUrl = "",
+      baseAuth: RouteMeta["auth"]
+    ): RouteRecordRaw[] {
+      const res: RouteRecordRaw[] = [];
       routes.forEach((route) => {
         if (route.children) {
-          const childrenBaseUrl = resolveRoutePath(baseUrl, route.path)
-          const childrenBaseAuth = !baseAuth || baseAuth === '' || baseAuth?.length === 0 ? route.meta?.auth : baseAuth
-          const tmpBreadcrumb = cloneDeep(breadcrumb)
+          const childrenBaseUrl = resolveRoutePath(baseUrl, route.path);
+          const childrenBaseAuth =
+            !baseAuth || baseAuth === "" || baseAuth?.length === 0
+              ? route.meta?.auth
+              : baseAuth;
+          const tmpBreadcrumb = cloneDeep(breadcrumb);
           tmpBreadcrumb.push({
             path: childrenBaseUrl,
             title: route.meta?.title,
@@ -47,96 +62,106 @@ const useRouteStore = defineStore(
             icon: route.meta?.icon,
             activeIcon: route.meta?.activeIcon,
             hide: !route.meta?.breadcrumb && route.meta?.breadcrumb === false,
-          })
-          const tmpRoute = cloneDeep(route)
-          tmpRoute.path = childrenBaseUrl
+          });
+          const tmpRoute = cloneDeep(route);
+          tmpRoute.path = childrenBaseUrl;
           if (!tmpRoute.meta) {
-            tmpRoute.meta = {}
+            tmpRoute.meta = {};
           }
-          tmpRoute.meta.auth = childrenBaseAuth
-          tmpRoute.meta.breadcrumbNeste = tmpBreadcrumb
-          delete tmpRoute.children
-          res.push(tmpRoute)
-          const childrenRoutes = flatAsyncRoutesRecursive(route.children, tmpBreadcrumb, childrenBaseUrl, childrenBaseAuth)
+          tmpRoute.meta.auth = childrenBaseAuth;
+          tmpRoute.meta.breadcrumbNeste = tmpBreadcrumb;
+          delete tmpRoute.children;
+          res.push(tmpRoute);
+          const childrenRoutes = flatAsyncRoutesRecursive(
+            route.children,
+            tmpBreadcrumb,
+            childrenBaseUrl,
+            childrenBaseAuth
+          );
           childrenRoutes.forEach((item) => {
             // 如果 path 一样则覆盖，因为子路由的 path 可能设置为空，导致和父路由一样，直接注册会提示路由重复
-            if (res.some(v => v.path === item.path)) {
+            if (res.some((v) => v.path === item.path)) {
               res.forEach((v, i) => {
                 if (v.path === item.path) {
-                  res[i] = item
+                  res[i] = item;
                 }
-              })
+              });
+            } else {
+              res.push(item);
             }
-            else {
-              res.push(item)
-            }
-          })
-        }
-        else {
-          const tmpRoute = cloneDeep(route)
-          tmpRoute.path = resolveRoutePath(baseUrl, tmpRoute.path)
+          });
+        } else {
+          const tmpRoute = cloneDeep(route);
+          tmpRoute.path = resolveRoutePath(baseUrl, tmpRoute.path);
           // 处理面包屑导航
-          const tmpBreadcrumb = cloneDeep(breadcrumb)
+          const tmpBreadcrumb = cloneDeep(breadcrumb);
           tmpBreadcrumb.push({
             path: tmpRoute.path,
             title: tmpRoute.meta?.title,
             i18n: tmpRoute.meta?.i18n,
             icon: tmpRoute.meta?.icon,
             activeIcon: tmpRoute.meta?.activeIcon,
-            hide: !tmpRoute.meta?.breadcrumb && tmpRoute.meta?.breadcrumb === false,
-          })
+            hide:
+              !tmpRoute.meta?.breadcrumb && tmpRoute.meta?.breadcrumb === false,
+          });
           if (!tmpRoute.meta) {
-            tmpRoute.meta = {}
+            tmpRoute.meta = {};
           }
-          tmpRoute.meta.auth = !baseAuth || baseAuth === '' || baseAuth?.length === 0 ? tmpRoute.meta?.auth : baseAuth
-          tmpRoute.meta.breadcrumbNeste = tmpBreadcrumb
-          res.push(tmpRoute)
+          tmpRoute.meta.auth =
+            !baseAuth || baseAuth === "" || baseAuth?.length === 0
+              ? tmpRoute.meta?.auth
+              : baseAuth;
+          tmpRoute.meta.breadcrumbNeste = tmpBreadcrumb;
+          res.push(tmpRoute);
         }
-      })
-      return res
+      });
+      return res;
     }
     // 扁平化路由（将三级及以上路由数据拍平成二级）
     const flatRoutes = computed(() => {
-      const returnRoutes: RouteRecordRaw[] = []
-      if (settingsStore.settings.app.routeBaseOn !== 'filesystem') {
+      const returnRoutes: RouteRecordRaw[] = [];
+      if (settingsStore.settings.app.routeBaseOn !== "filesystem") {
         if (routesRaw.value) {
           routesRaw.value.forEach((item) => {
-            const tmpRoutes = cloneDeep(item.children) as RouteRecordRaw[]
+            const tmpRoutes = cloneDeep(item.children) as RouteRecordRaw[];
             tmpRoutes.map((v) => {
               if (!v.meta) {
-                v.meta = {}
+                v.meta = {};
               }
-              v.meta.auth = item.meta?.auth ?? v.meta?.auth
-              return v
-            })
-            returnRoutes.push(...tmpRoutes)
-          })
-          returnRoutes.forEach(item => flatAsyncRoutes(item))
+              v.meta.auth = item.meta?.auth ?? v.meta?.auth;
+              return v;
+            });
+            returnRoutes.push(...tmpRoutes);
+          });
+          returnRoutes.forEach((item) => flatAsyncRoutes(item));
         }
+      } else {
+        returnRoutes.push(
+          ...(cloneDeep(filesystemRoutesRaw.value) as RouteRecordRaw[])
+        );
       }
-      else {
-        returnRoutes.push(...cloneDeep(filesystemRoutesRaw.value) as RouteRecordRaw[])
-      }
-      return returnRoutes
-    })
+      return returnRoutes;
+    });
     const flatSystemRoutes = computed(() => {
-      const routes = [...systemRoutes]
-      routes.forEach(item => flatAsyncRoutes(item))
-      return routes
-    })
+      const routes = [...systemRoutes];
+      routes.forEach((item) => flatAsyncRoutes(item));
+      return routes;
+    });
 
     // 将设置 meta.singleMenu 的一级路由转换成二级路由
-    function convertSingleRoutes<T extends Route.recordMainRaw[]>(routes: T): T {
+    function convertSingleRoutes<T extends Route.recordMainRaw[]>(
+      routes: T
+    ): T {
       routes.map((route) => {
         if (route.children) {
           route.children.forEach((item, index, arr) => {
             if (item.meta?.singleMenu) {
               arr[index] = {
                 ...item,
-                component: () => import('@/layouts/index.vue'),
+                component: () => import("@/layouts/index.vue"),
                 children: [
                   {
-                    path: '',
+                    path: "",
                     component: item.component,
                     meta: {
                       title: item.meta.title,
@@ -146,60 +171,66 @@ const useRouteStore = defineStore(
                     },
                   },
                 ],
-              } as RouteRecordRaw
-              delete arr[index].meta!.singleMenu
+              } as RouteRecordRaw;
+              delete arr[index].meta!.singleMenu;
             }
-          })
+          });
         }
-        return route
-      })
-      return routes
+        return route;
+      });
+      return routes;
     }
 
     // TODO 将设置 meta.sidebar 的属性转换成 meta.menu ，过渡处理，未来将被弃用
-    let isUsedDeprecatedAttribute = false
-    function converDeprecatedAttribute<T extends Route.recordMainRaw[]>(routes: T): T {
+    let isUsedDeprecatedAttribute = false;
+    function converDeprecatedAttribute<T extends Route.recordMainRaw[]>(
+      routes: T
+    ): T {
       routes.forEach((route) => {
-        route.children = converDeprecatedAttributeRecursive(route.children)
-      })
+        route.children = converDeprecatedAttributeRecursive(route.children);
+      });
       if (isUsedDeprecatedAttribute) {
         // turbo-console-disable-next-line
-        console.warn('[Fantastic-admin] 路由配置中的 "sidebar" 属性即将被弃用, 请尽快替换为 "menu" 属性')
+        console.warn(
+          '[Fantastic-admin] 路由配置中的 "sidebar" 属性即将被弃用, 请尽快替换为 "menu" 属性'
+        );
       }
-      return routes
+      return routes;
     }
     function converDeprecatedAttributeRecursive(routes: RouteRecordRaw[]) {
       if (routes) {
         routes.forEach((route) => {
-          if (typeof route.meta?.sidebar === 'boolean') {
-            isUsedDeprecatedAttribute = true
-            route.meta.menu = route.meta.sidebar
-            delete route.meta.sidebar
+          if (typeof route.meta?.sidebar === "boolean") {
+            isUsedDeprecatedAttribute = true;
+            route.meta.menu = route.meta.sidebar;
+            delete route.meta.sidebar;
           }
           if (route.children) {
-            converDeprecatedAttributeRecursive(route.children)
+            converDeprecatedAttributeRecursive(route.children);
           }
-        })
+        });
       }
-      return routes
+      return routes;
     }
 
     // 生成路由（前端生成）
     function generateRoutesAtFront(asyncRoutes: Route.recordMainRaw[]) {
       // 设置 routes 数据
-      routesRaw.value = converDeprecatedAttribute(convertSingleRoutes(cloneDeep(asyncRoutes) as any))
-      isGenerate.value = true
+      routesRaw.value = converDeprecatedAttribute(
+        convertSingleRoutes(cloneDeep(asyncRoutes) as any)
+      );
+      isGenerate.value = true;
       // 加载常驻标签页
       if (settingsStore.settings.tabbar.enable) {
-        tabbarStore.initPermanentTab()
+        tabbarStore.initPermanentTab();
       }
     }
     // 路由排序
-    function recursiveSort(array:any) {
+    function recursiveSort(array: any) {
       // 首先对当前层级的数组进行排序
-      array.sort((a:any, b:any) => a.sort - b.sort);
+      array.sort((a: any, b: any) => a.sort - b.sort);
       // 遍历当前层级的每个对象
-      array.forEach((item:any) => {
+      array.forEach((item: any) => {
         // 如果当前对象包含 children 属性且 children 是一个数组
         if (item.children && Array.isArray(item.children)) {
           // 递归调用函数对 children 进行排序和处理
@@ -209,63 +240,91 @@ const useRouteStore = defineStore(
       return array;
     }
     // 格式化后端路由数据
-    function formatBackRoutes(routes: any, views = import.meta.glob('../../views/**/*.vue')): Route.recordMainRaw[] {
+    function formatBackRoutes(
+      routes: any,
+      views = import.meta.glob("../../views/**/*.vue")
+    ): Route.recordMainRaw[] {
       return routes.map((route: any) => {
         switch (route.component) {
-          case 'Layout':
-            route.component = () => import('@/layouts/index.vue')
-            break
+          case "Layout":
+            route.component = () => import("@/layouts/index.vue");
+            break;
           default:
             if (route.component) {
-              route.component = views[`../../views/${route.component}`]
-            }
-            else {
-              delete route.component
+              route.component = views[`../../views/${route.component}`];
+            } else {
+              delete route.component;
             }
         }
         if (route.children) {
-          route.children = formatBackRoutes(route.children, views)
+          route.children = formatBackRoutes(route.children, views);
         }
-        return route
-      })
+        return route;
+      });
     }
     // 生成路由（后端获取）
     async function generateRoutesAtBack() {
-      await apiApp.routeList().then((res: any) => {
-        // 设置 routes 数据
-        const dataList = converDeprecatedAttribute(convertSingleRoutes(formatBackRoutes(res.data) as any))
-        // 将路由数据放入递归函数中排序
-        routesRaw.value = recursiveSort(dataList)
-        isGenerate.value = true
-        // 初始化常驻标签页
-        if (settingsStore.settings.tabbar.enable) {
-          tabbarStore.initPermanentTab()
-        }
-      }).catch(() => { })
+      await apiApp
+        .routeList()
+        .then((res: any) => {
+          // 设置 routes 数据
+          const dataList = converDeprecatedAttribute(
+            convertSingleRoutes(formatBackRoutes(res.data) as any)
+          );
+          // 将路由数据放入递归函数中排序
+          routesRaw.value = recursiveSort(dataList);
+          isGenerate.value = true;
+          // 初始化常驻标签页
+          if (settingsStore.settings.tabbar.enable) {
+            tabbarStore.initPermanentTab();
+          }
+        })
+        .catch(() => {});
     }
     // 生成路由（文件系统生成）
     function generateRoutesAtFilesystem(asyncRoutes: RouteRecordRaw[]) {
       // 设置 routes 数据
-      filesystemRoutesRaw.value = convertSingleRoutes(cloneDeep(asyncRoutes) as any)
-      isGenerate.value = true
+      filesystemRoutesRaw.value = convertSingleRoutes(
+        cloneDeep(asyncRoutes) as any
+      );
+      isGenerate.value = true;
       // 加载常驻标签页
       if (settingsStore.settings.tabbar.enable) {
-        tabbarStore.initPermanentTab()
+        tabbarStore.initPermanentTab();
       }
     }
     // 记录 accessRoutes 路由，用于登出时删除路由
     function setCurrentRemoveRoutes(routes: (() => void)[]) {
-      currentRemoveRoutes.value = routes
+      currentRemoveRoutes.value = routes;
     }
     // 清空动态路由
     function removeRoutes() {
-      isGenerate.value = false
-      routesRaw.value = []
-      filesystemRoutesRaw.value = []
+      isGenerate.value = false;
+      routesRaw.value = [];
+      filesystemRoutesRaw.value = [];
       currentRemoveRoutes.value.forEach((removeRoute) => {
-        removeRoute()
-      })
-      currentRemoveRoutes.value = []
+        removeRoute();
+      });
+      currentRemoveRoutes.value = [];
+    }
+
+    // 扁平化路由
+    function flatRouter(menu: any, parentPath = "", flattened: any = []) {
+      menu.forEach((item: any) => {
+        const { children, ...flattenedItem } = item;
+        flattened.push(flattenedItem);
+        if (item.children) {
+          flatRouter(item.children, parentPath + item.path, flattened);
+        }
+      });
+      return flattened;
+    }
+    // 扁平化路由 获取1，2级路由
+    async function obtainLevel1AndLevel2Routing() {
+      const flatRouterList = await flatRouter(cloneDeep(routesRaw.value));
+      return flatRouterList.filter((item: any) => {
+        return item.menuLevel === 1 || item.menuLevel === 2;
+      });
     }
 
     return {
@@ -279,8 +338,10 @@ const useRouteStore = defineStore(
       generateRoutesAtFilesystem,
       setCurrentRemoveRoutes,
       removeRoutes,
-    }
-  },
-)
+      flatRouter,
+      obtainLevel1AndLevel2Routing,
+    };
+  }
+);
 
-export default useRouteStore
+export default useRouteStore;
--- src/views/configuration/role/components/DetailForm/index.vue
+++ src/views/configuration/role/components/DetailForm/index.vue
@@ -1,25 +1,26 @@
 <script setup lang="ts">
-import type { FormInstance, FormRules } from 'element-plus'
-import { ElMessage } from 'element-plus'
-import api from '@/api/modules/configuration_role'
-import useRouteStore from '@/store/modules/route'
-import useRoleButtonStore from '@/store/modules/get_role_button'
+import type { FormInstance, FormRules } from "element-plus";
+import { ElMessage } from "element-plus";
+import api from "@/api/modules/configuration_role";
+import useRouteStore from "@/store/modules/route";
+import useRoleButtonStore from "@/store/modules/get_role_button";
+import cloneDeep from "lodash-es/cloneDeep";
 
 // 父级传递数据
-const props = defineProps(['id', 'row'])
+const props = defineProps(["id", "row"]);
 // 路由 store
-const routeStore = useRouteStore()
+const routeStore = useRouteStore();
 // 按钮权限store
-const roleButton = useRoleButtonStore()
+const roleButton = useRoleButtonStore();
 // 加载
-const loading = ref(false)
-const formRef = ref<FormInstance>()
+const loading = ref(false);
+const formRef = ref<FormInstance>();
 // tree ref
-const treeRef = ref<any>()
+const treeRef = ref<any>();
 // 路由
-const menuData = ref<any>([])
+const menuData = ref<any>([]);
 // 权限
-const permissionData = ref<any>([])
+const permissionData = ref<any>([]);
 // 定义表单
 const form = ref<any>({
   id: props.id ?? null,
@@ -28,92 +29,128 @@ const form = ref<any>({
   // 按钮
   permission: [],
   // 角色码
-  roleName: '',
+  roleName: "",
   // 备注
-  remark: '',
-})
+  remark: "",
+});
 // 校验
 const formRules = ref<FormRules>({
-  roleName: [
-    { required: true, message: '请输入角色吗', trigger: 'blur' },
-  ],
-})
+  roleName: [{ required: true, message: "请输入角色吗", trigger: "blur" }],
+});
 
 onMounted(async () => {
-  if (form.value.id !== '') {
-    await getInfo()
+  loading.value = true;
+  if (form.value.id !== "") {
+    await getInfo();
   }
-  loading.value = true
+
   // 从store获取原始路由
-  menuData.value = routeStore.routesRaw
+  menuData.value = routeStore.routesRaw;
+  // 获取扁平化后的1，2级路由
+  const Level1AndLevel2List = await routeStore.obtainLevel1AndLevel2Routing();
+  // 只保留第三级路由
+  form.value.menuId = form.value.menuId.filter((item: any) => {
+    return !Level1AndLevel2List.some((ite: any) => ite.id === item);
+  });
   // 调用store的方法获取按钮权限，如果没有就调接口
-  permissionData.value = await roleButton.getPermissions
-  loading.value = false
-})
+  permissionData.value = await roleButton.getPermissions;
+  loading.value = false;
+});
 
 // 获取
 async function getInfo() {
-  loading.value = true
+  loading.value = true;
   // 编辑时获取该id的具体数据
-  form.value = JSON.parse(props.row)
-  loading.value = false
+  form.value = JSON.parse(props.row);
+  // loading.value = false;
 }
 
 // 查询当前路由有那些权限
 function rowPermission(permissionID: any) {
-  return permissionData?.value?.filter((item: any) => permissionID === item.menuId)
+  return permissionData?.value?.filter(
+    (item: any) => permissionID === item.menuId
+  );
 }
 // 暴露
 defineExpose({
   submit() {
     // 同步选中的路由id
-    form.value.menuId = treeRef.value!.getCheckedKeys(false)
+    form.value.menuId = treeRef.value!.getCheckedKeys(false);
     return new Promise<void>((resolve) => {
-      if (form.value.id === '') {
-        formRef.value && formRef.value.validate((valid: any) => {
-          if (valid) {
-            api.create(form.value).then(() => {
-              ElMessage.success({
-                message: '新增成功',
-                center: true,
-              })
-              resolve()
-            })
-          }
-        })
+      //  获取选中的所有子节点
+      const tree = treeRef.value.getCheckedKeys();
+      // 获取所有半选的主节点
+      const halltree = treeRef.value.getHalfCheckedKeys();
+      // 组合一下
+      const menupath = tree.concat(halltree);
+      form.value.menuId = menupath;
+      // return;
+      if (form.value.id === "") {
+        formRef.value &&
+          formRef.value.validate((valid: any) => {
+            if (valid) {
+              api.create(form.value).then(() => {
+                ElMessage.success({
+                  message: "新增成功",
+                  center: true,
+                });
+                resolve();
+              });
+            }
+          });
+      } else {
+        formRef.value &&
+          formRef.value.validate((valid: any) => {
+            if (valid) {
+              api.edit(form.value).then(() => {
+                ElMessage.success({
+                  message: "编辑成功",
+                  center: true,
+                });
+                resolve();
+              });
+            }
+          });
       }
-      else {
-        formRef.value && formRef.value.validate((valid: any) => {
-          if (valid) {
-            api.edit(form.value).then(() => {
-              ElMessage.success({
-                message: '编辑成功',
-                center: true,
-              })
-              resolve()
-            })
-          }
-        })
-      }
-    })
+    });
   },
-})
+});
 </script>
 
 <template>
   <div v-loading="loading">
-    <ElForm ref="formRef" :model="form" :rules="formRules" label-width="120px" label-suffix="：">
+    <ElForm
+      ref="formRef"
+      :model="form"
+      :rules="formRules"
+      label-width="120px"
+      label-suffix="："
+    >
       <ElFormItem label="角色码" prop="roleName">
-        <ElInput v-model="form.roleName" :disabled="!!form.id" placeholder="请输入角色码" />
+        <ElInput
+          v-model="form.roleName"
+          :disabled="!!form.id"
+          placeholder="请输入角色码"
+        />
       </ElFormItem>
       <ElFormItem label="备注" prop="remark">
         <ElInput v-model="form.remark" placeholder="请输入备注" />
       </ElFormItem>
       <ElFormItem label="权限">
         <el-tree
-          ref="treeRef" :data="menuData" style="width: 100%;" :default-checked-keys="form.menuId"
-          :default-expanded-keys="[]" node-key="id" show-checkbox default-expand-all highlight-current border
+          v-if="!loading"
+          ref="treeRef"
+          :data="menuData"
+          style="width: 100%"
+          :default-checked-keys="form.menuId"
+          :default-expanded-keys="[]"
+          node-key="id"
+          show-checkbox
+          default-expand-all
+          border
         >
+          <!--          check-strictly
+          highlight-current -->
           <template #default="{ data }">
             <div class="custom-tree-node">
               <div class="menu">
@@ -122,7 +159,11 @@ defineExpose({
               <div class="permission">
                 <div v-if="rowPermission(data.id)?.length" class="permissions">
                   <ElCheckboxGroup v-model="form.permission">
-                    <ElCheckbox v-for="auth in rowPermission(data.id)" :key="auth.id" :value="auth.id">
+                    <ElCheckbox
+                      v-for="auth in rowPermission(data.id)"
+                      :key="auth.id"
+                      :value="auth.id"
+                    >
                       {{ auth.label }}
                     </ElCheckbox>
                   </ElCheckboxGroup>
