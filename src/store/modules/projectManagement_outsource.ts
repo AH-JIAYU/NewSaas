914b02996d9b4abac0362a0b684c8e6196f93638
llg <3102093662@qq.com>
2024-08-16 20:42:56 +0800

    项目外包-测查

--- src/store/modules/projectManagement_outsource.ts
+++ src/store/modules/projectManagement_outsource.ts
@@ -5,10 +5,18 @@ const useProjectManagementOutsourceStore = defineStore(
     // 项目装填
     const projectStatusList = ["进行中(在线)", "已完成(审核通过)", "离线"];
     const typeList = ["租户", "供应商", "会员组"];
+    const surveyStatusList = [
+      '完成',
+      '被甄别',
+      '配额满',
+      '安全终止',
+      '未完成'
+    ]
 
     return {
       projectStatusList,
       typeList,
+      surveyStatusList,
     };
   }
 );
--- src/views/projectManagement/outsource/components/Edit/index.vue
+++ src/views/projectManagement/outsource/components/Edit/index.vue
@@ -3,6 +3,7 @@ import api from "@/api/modules/projectManagement_outsource";
 import { ElMessage } from "element-plus";
 import { cloneDeep } from "lodash-es";
 import useProjectManagementOutsourceStore from "@/store/modules/projectManagement_outsource";
+import { throttle } from "lodash-es";
 
 defineOptions({
   name: "Edit",
@@ -11,9 +12,10 @@ const projectManagementOutsourceStore = useProjectManagementOutsourceStore();
 // 弹框开关变量
 const dialogTableVisible = ref(false);
 const data = ref<any>({
-  initialTenantCurrencyType: "", // 初始租户的货币类型
+
   currentTenantId: "", //当前租户id
   tenantMeasurementInfoList: [], //测查列表
+  clickIdList: [],//点击id列表
 });
 
 // 显隐
@@ -23,30 +25,74 @@ async function showEdit(row: any) {
     projectId: row.projectId,
   };
   const res = await api.getTenantMeasurementList(params);
-  // 获取初始租户的货币类型
-  data.value.initialTenantCurrencyType =
-    res.data.tenantMeasurementInfoList[0].currencyType;
+
   // 过滤数据（删除与当前租户id相同的数据之前的所有数据，包括当前的数据）
   const findDataIndex = res.data.tenantMeasurementInfoList.findIndex(
     (item: any) => item.allocationTenantId === res.data.currentTenantId
   );
-  res.data.tenantMeasurementInfoList.splice(0, findDataIndex);
+  const resList = res.data.tenantMeasurementInfoList
+  resList.splice(0, findDataIndex);//删除上级
+  const type1List = resList.filter((item: any) => item.type === 1)
+  const type2List = resList.filter((item: any) => item.type === 2)
+  const type3List = resList.filter((item: any) => item.type === 3)
+  const tenantMeasurementInfoList = [
+    ...type1List
+  ]
+  if (type2List.length) {
+    const memberGroupOrSupperIdList = type2List.map((item: any) => item.memberGroupOrSupperId);
+    type2List[0].length = type2List.length
+    type2List[0].memberGroupOrSupperIdList = memberGroupOrSupperIdList
+    tenantMeasurementInfoList.push(type2List[0])
+  } else {
+    const memberGroupOrSupperIdList = type3List.map((item: any) => item.memberGroupOrSupperId);
+    type3List[0].length = type3List.length
+    type3List[0].memberGroupOrSupperIdList = memberGroupOrSupperIdList
+    tenantMeasurementInfoList.push(type3List[0])
+  }
   data.value.currentTenantId = res.data.currentTenantId;
-  data.value.tenantMeasurementInfoList = res.data.tenantMeasurementInfoList;
+  data.value.tenantMeasurementInfoList = tenantMeasurementInfoList;
   dialogTableVisible.value = true;
 }
 // 获取点击id
-async function getClickList(row: any) {
-  const params = {
-    type: row.type,
-    projectId: row.projectId,
-    tenantId: row.tenantId,
-    supplierId: row.type === 2 ? row.memberGroupOrSupperId : "",
-    memberGroupId: row.type === 3 ? row.memberGroupOrSupperId : "",
-  };
-  const res = await api.getQuestionnaireClickList(params);
-  console.log("res", res.data.questionnaireClickInfoListz);
+const getClickList = async (row: any) => {
+  if (!row.visible) {
+    const params = {
+      type: row.type,
+      projectId: row.projectId,
+      tenantId: row.allocationTenantId,
+      supplierIdList: row.type === 2 ? row.memberGroupOrSupperIdList : [],
+      memberGroupIdList: row.type === 3 ? row.memberGroupOrSupperIdList : [],
+    };
+    const res = await api.getQuestionnaireClickList(params);
+    data.value.clickIdList = processData(res.data.questionnaireClickInfoList) 
+    row.visible = true
+  }
 }
+
+// 处理数据
+function processData(data: any) {
+  return data.reduce((acc: any, item: any) => {
+    const { supplierId, supplierName, projectQuestionnaireClickId, surveyStatus, price } = item;
+
+    // 查找是否已经有该供应商的对象
+    let supplier = acc.find((s: any) => s.supplierId === supplierId);
+
+    if (supplier) {
+      // 如果供应商已经存在，直接将数据添加到其 list 中
+      supplier.list.push({ projectQuestionnaireClickId, surveyStatus, price });
+    } else {
+      // 如果供应商不存在，创建新的供应商对象并添加到结果中
+      acc.push({
+        supplierId,
+        supplierName,
+        list: [{ projectQuestionnaireClickId, surveyStatus, price }]
+      });
+    }
+
+    return acc;
+  }, []);
+}
+
 // 取消
 function closeHandler() {
   dialogTableVisible.value = false;
@@ -61,38 +107,27 @@ defineExpose({ showEdit });
 
 <template>
   <div>
-    <el-drawer
-      v-model="dialogTableVisible"
-      title="项目测查"
-      size="60%"
-      :before-close="closeHandler"
-    >
+    <el-drawer v-model="dialogTableVisible" title="项目测查" size="60%" :before-close="closeHandler">
       <div class="milestone">
-        <div
-          class="item"
-          v-for="(item, index) in data.tenantMeasurementInfoList"
-          :key="item.allocationTenantId"
-        >
+        <div class="item" v-for="(item, index) in data.tenantMeasurementInfoList" :key="item.allocationTenantId">
           <div class="left">
-            <div
-              :class="{
-                'left-top': true,
-                'flex-b': true,
-                currentTenant:
-                  item.allocationTenantId === data.currentTenantId &&
-                  item.type === 1,
-              }"
-            >
+            <div :class="{
+      'left-top': true,
+      'flex-b': true,
+      currentTenant:
+        item.allocationTenantId === data.currentTenantId &&
+        item.type === 1,
+    }">
               <div class="left-top-left p-4">
                 <div class="project">
                   <div class="projectName flex-b">
                     <b>{{ item.projectName }}</b>
                     <span>
                       {{
-                        projectManagementOutsourceStore.projectStatusList[
-                          item.projectStatus - 1
-                        ]
-                      }}
+      projectManagementOutsourceStore.projectStatusList[
+      item.projectStatus - 1
+      ]
+    }}
                     </span>
                   </div>
                   <div class="projectId">{{ item.projectId }}</div>
@@ -102,14 +137,10 @@ defineExpose({ showEdit });
                     原价：
                     <CurrencyType />
                     {{ item.doMoneyPrice }}
-
-                    <!-- {{ item.currencyType }} -->
-                    <!-- {{ item.type }} -->
                   </p>
                   <p>
                     参数：
-                    <el-text size="large"
-                      >{{ item.participationNumber || 0 }}
+                    <el-text size="large">{{ item.participationNumber || 0 }}
                     </el-text>
                     <el-text size="large"> / </el-text>
                     <el-text type="success" size="large">
@@ -117,64 +148,81 @@ defineExpose({ showEdit });
                     </el-text>
                     <el-text size="large"> / </el-text>
                     <el-text type="warning" size="large">
-                      {{ item.num || 0 }}</el-text
-                    ><el-text size="large"> / </el-text>
+                      {{ item.num || 0 }}</el-text><el-text size="large"> / </el-text>
                     <el-text size="large">{{
-                      item.limitedQuantity || 0
-                    }}</el-text>
+      item.limitedQuantity || 0
+    }}</el-text>
                   </p>
                 </div>
               </div>
-              <div
-                class="left-top-right p-4"
-                v-if="
-                  item.allocationTenantId !== data.currentTenantId ||
-                  item.type !== 1
-                "
-              >
-                <p class="type">
+              <div class="left-top-right p-4 " v-if="item.allocationTenantId !== data.currentTenantId ||
+      item.type !== 1
+      ">
+                <p :class="'type' + item.type">
                   {{ projectManagementOutsourceStore.typeList[item.type - 1] }}
                 </p>
                 <div class="tenant">
-                  <p>{{ item.tenantName }}</p>
-                  <p>{{ item.allocationTenantId }}</p>
+                  <template v-if="item.length > 1">
+                    <el-button type="primary" link @click="getClickList(item)">
+                      查看更多({{ item.length }})
+                    </el-button>
+                  </template>
+                  <template v-else>
+                    <p>{{ item.tenantName }}</p>
+                    <p>{{ item.allocationTenantId }}</p>
+                  </template>
+
                 </div>
               </div>
             </div>
-            <div
-              class="left-bottom"
-              v-if="data.tenantMeasurementInfoList.length !== index + 1"
-            >
+            <div class="left-bottom" v-if="data.tenantMeasurementInfoList.length !== index + 1">
               <div class="line"></div>
               <div class="allocation">
                 {{ item.tenantName }}-分配-{{
-                  projectManagementOutsourceStore.typeList[
-                    data.tenantMeasurementInfoList[index + 1].type - 1
-                  ]
-                }}
+      projectManagementOutsourceStore.typeList[
+      data.tenantMeasurementInfoList[index + 1].type - 1
+      ]
+    }}
               </div>
               <div class="arrow"></div>
             </div>
           </div>
-          <div
-            class="right"
-            v-if="
-              item.allocationTenantId !== data.currentTenantId || item.type != 1
-            "
-          >
-            <el-popover
-              placement="right"
-              :width="300"
-              trigger="click"
-              @before-enter="getClickList(item)"
-            >
+          <div class="right" v-if="item.allocationTenantId !== data.currentTenantId || item.type != 1
+      ">
+            <el-popover v-model:visible="item.visible" placement="right" :width="350" trigger="click"
+              @show="getClickList(item)">
               <template #reference>
-                <!-- <el-button style="margin-right: 16px"> </el-button> -->
                 <el-button style="padding: 0.5rem" type="primary" link>
                   》
                 </el-button>
               </template>
-              1111
+
+              <div class="clickIdItem   m-2" v-for="ite in data.clickIdList">
+                <div class="clickIdItem-left">
+                  <span :class="'type' + item.type">
+                    {{ projectManagementOutsourceStore.typeList[item.type - 1] }}
+                  </span>
+                </div>
+                <div class="clickIdItem-right">
+                  <div class="title">
+                    {{ ite.supplierName }} &emsp; {{ ite.supplierId }}
+                  </div>
+                  <ul>
+                    <li>点击ID:</li>
+                    <li v-for="it in ite.list">
+                      <span> {{ it.projectQuestionnaireClickId }}</span>
+                      <span> &emsp;{{ projectManagementOutsourceStore.surveyStatusList[it.surveyStatus - 1] }}</span>
+                      <span v-if="it.surveyStatus===1">
+                        &emsp;
+                        <CurrencyType />{{it.price}}
+                      </span>
+
+                    </li>
+                  </ul>
+                </div>
+
+
+              </div>
             </el-popover>
           </div>
         </div>
@@ -190,26 +238,44 @@ defineExpose({ showEdit });
   </div>
 </template>
 <style lang="scss" scoped>
+.type1 {
+  background-color: var(--el-color-primary);
+}
+
+.type2 {
+  background-color: var(--el-color-success);
+  ;
+}
+
+.type3 {
+  background-color: var(--el-color-warning);
+}
+
 .flex-b {
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
+
 .milestone {
   width: 50%;
   margin: auto;
 }
+
 .item {
   height: 15.625rem;
   display: flex;
   justify-content: space-around;
   align-items: start;
+
   .left {
     height: 100%;
     flex: 1;
-    > div {
+
+    >div {
       height: 50%;
     }
+
     .currentTenant {
       border: 1px dashed var(--el-color-primary) !important;
       width: 70%;
@@ -224,26 +290,31 @@ defineExpose({ showEdit });
       .left-top-left {
         width: 70%;
       }
+
       .left-top-right {
         width: 30%;
         border-left: 1px dashed #ccc;
       }
-      > div {
+
+      >div {
         height: 100%;
         display: flex;
         flex-direction: column;
         justify-content: space-between;
         align-items: start;
-        > div {
+
+        >div {
           width: 100%;
         }
       }
     }
+
     .left-bottom {
       display: flex;
       flex-direction: column;
       justify-content: center;
       align-items: center;
+
       .line,
       .arrow {
         flex: 1;
@@ -251,25 +322,55 @@ defineExpose({ showEdit });
         background-color: var(--el-color-primary);
         position: relative;
       }
+
       .arrow::after {
         content: "";
         display: block;
         position: absolute;
-        right: 50%; /* 箭头位置 */
-        bottom: 0; /* 箭头位置 */
+        right: 50%;
+        /* 箭头位置 */
+        bottom: 0;
+        /* 箭头位置 */
         transform: translateX(50%);
-        border-top: 0.625rem solid var(--el-color-primary); /* 箭头高低 */
-        border-right: 0.625rem solid transparent; /* 箭头高低 */
-        border-left: 0.625rem solid transparent; /* 箭头长度*/
+        border-top: 0.625rem solid var(--el-color-primary);
+        /* 箭头高低 */
+        border-right: 0.625rem solid transparent;
+        /* 箭头高低 */
+        border-left: 0.625rem solid transparent;
+        /* 箭头长度*/
       }
     }
   }
+
   .right {
     height: 50%;
     line-height: calc(15.625rem / 2);
     width: 2rem;
     text-align: center;
     color: var(--el-color-primary);
+
   }
+}
+
+.clickIdItem {
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
+
+  .clickIdItem-left {
+    width:  3.125rem;
+  }
+
+  .clickIdItem-right {
+    flex: 1;
+
+
+    ul {
+      li {
+        margin: .3125rem 0;
+      }
+    }
+  }
+
 }
 </style>
