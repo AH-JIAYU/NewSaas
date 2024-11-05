fa54023dbdd38684e86e56cec791d0a987e0cd80
古德猫宁 <2426104560@qq.com>
2024-11-05 18:05:21 +0800

    记录变更

--- src/views/record/memberSurveyRecords/index.vue
+++ src/views/record/memberSurveyRecords/index.vue
@@ -524,7 +524,7 @@ function handleCurrentChange(val: any) {
               <div v-if="row.viceStatus">
                 <el-tag effect="dark" style="background-color: #74868b; border: none" v-if="row.viceStatus === 1"
                   class="mx-1" type="success">待审</el-tag>
-                <el-tag effect="dark" style="background-color: #475061; border: none" v-if="row.viceStatus === 2"
+                <el-tag effect="dark" style="background-color: #03c239; border: none" v-if="row.viceStatus === 2"
                   class="mx-1" type="danger">完成</el-tag>
                 <el-tag effect="dark" style="background-color: #3e5c78; border: none" v-if="row.viceStatus === 3"
                   class="mx-1" type="primary">过IR</el-tag>
@@ -549,9 +549,9 @@ function handleCurrentChange(val: any) {
                 <el-tag effect="dark" style="background-color: #03c239; border: none" v-if="row.viceStatus === 13"
                   class="mx-1">和解</el-tag>
               </div>
-              <el-text v-else></el-text>
+              <el-text v-else>-</el-text>
             </div>
-            <el-text v-else></el-text>
+            <el-text v-else>-</el-text>
           </template>
         </ElTableColumn>
         <template #empty>
