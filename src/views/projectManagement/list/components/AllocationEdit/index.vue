0016c254e90e37080dfdd31068a60cfb02a85e1d
古德猫宁 <2426104560@qq.com>
2024-12-12 19:24:52 +0800

    修改项目分配样式

--- src/views/projectManagement/list/components/AllocationEdit/index.vue
+++ src/views/projectManagement/list/components/AllocationEdit/index.vue
@@ -248,7 +248,7 @@ defineExpose({ showEdit });
           prop="clientName"
         />
       </el-table>
-      <el-form
+      <!-- <el-form
         ref="formRef"
         label-width="90px"
         :rules="rules"
@@ -403,9 +403,9 @@ defineExpose({ showEdit });
           </template>
 
         </el-form-item>
-      </el-form>
+      </el-form> -->
 
-      <!-- <el-form ref="formRef" label-width="80px" :rules="rules" :model="data.form" :inline="false">
+      <el-form ref="formRef" label-width="80px" :rules="rules" :model="data.form" :inline="false">
         <el-form-item label="分配目标">
           <el-radio-group v-model="data.form.allocationType" class="ml-4" @change="changeRadio">
             <el-radio :value="2" size="large"> 供应商 </el-radio>
@@ -448,7 +448,7 @@ defineExpose({ showEdit });
               :value="item.beInvitationTenantId" :disabled="item.reveal === 1" />
           </el-select>
         </el-form-item>
-    </el-form> -->
+    </el-form>
 
       <template #footer>
         <div style="flex: auto">
