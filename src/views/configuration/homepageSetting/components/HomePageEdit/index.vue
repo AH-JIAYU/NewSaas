1b81ebf26d8a72d89bd41aea5c368225ae9757f8
wsx <1487444984@qq.com>
2025-02-13 17:12:33 +0800

    修改站点设置模版设计

--- src/views/configuration/homepageSetting/components/HomePageEdit/index.vue
+++ src/views/configuration/homepageSetting/components/HomePageEdit/index.vue
@@ -1,111 +1,128 @@
-<template>
-  <el-drawer v-if="state.dialogFormVisible" v-model="state.dialogFormVisible" :title="state.title" size="100%"
-    @opened="onDialogOpened" @close="close">
-    <div ref="formRef" id="gjs"></div>
-    <template #footer>
-      <el-button @click="close">取 消</el-button>
-      <el-button v-if="state.title === '设计模板'" type="primary" @click="save">确 定</el-button>
-    </template>
-  </el-drawer>
-</template>
-
 <script setup lang="ts">
-import grapesjs from "grapesjs";
-import plugin from "grapesjs-preset-webpage";
-import basic from "grapesjs-blocks-basic";
-// @ts-ignore
-import advance from "grapesjs-advance-components";
-import parserPostCSS from "grapesjs-parser-postcss";
-import "grapesjs/dist/css/grapes.min.css";
-import "grapesjs/dist/grapes.min.js";
-// @ts-ignore
-import zh from "grapesjs/locale/zh";
-import { updataText, customBlock, htmlJsList } from "@/utils/homePage";
-import api from "@/api/modules/configuration_homepageSetting";
-import useUserStore from "@/store/modules/user";
-import { ElMessage, ElMessageBox } from "element-plus";
-import { resolve } from "path-browserify";
+import grapesjs from 'grapesjs'
+import basic from 'grapesjs-blocks-basic'
+import gjsForms from 'grapesjs-plugin-forms'
+import * as tabs from 'grapesjs-tabs'
+import customCodePlugin from 'grapesjs-custom-code'
+import pluginCountdown from 'grapesjs-component-countdown'
+import parserPostCSS from 'grapesjs-parser-postcss'
+import presetWebpage from 'grapesjs-preset-webpage'
+import pluginTooltip from 'grapesjs-tooltip'
+import imageEditor from 'grapesjs-tui-image-editor'
+import styleBg from 'grapesjs-style-bg'
+// @ts-expect-error
+import 'grapesjs/dist/css/grapes.min.css'
+import 'grapesjs/dist/grapes.min.js'
+// @ts-expect-error
+import zh from 'grapesjs/locale/zh'
+import { ElMessage, ElMessageBox } from 'element-plus'
+import { customBlock, htmlJsList, updataText } from '@/utils/homePage'
+import api from '@/api/modules/configuration_homepageSetting'
 
+// 更新列表
+const emits = defineEmits(['fetch-data'])
 // 表单数据
 const state = reactive<any>({
-  title: "",
+  title: '',
   dialogFormVisible: false,
   form: {},
-});
-// 更新列表
-const emits = defineEmits(["fetch-data"]);
+})
 // token
-const userStore = useUserStore();
-const editorRef = ref<any>(null);
-const formRef = ref<any>(null);
+const editorRef = ref<any>(null)
+const formRef = ref<any>(null)
 
 // 打开弹框
-const showEdit = (row: any, title: any = "设计模板") => {
-  state.title = title;
-  state.form = row;
-  state.dialogFormVisible = true;
-};
+function showEdit(row: any, title: any = '设计模板') {
+  state.title = title
+  state.form = row
+  state.dialogFormVisible = true
+}
 
 // 打开grapesjs
-const onDialogOpened = async () => {
-  await nextTick();
+async function onDialogOpened() {
+  await nextTick()
   editorRef.value = grapesjs.init({
-    container: formRef.value,
-    height: "900px",
-    width: "100%",
-    plugins: [plugin, basic, advance, parserPostCSS],
+    fromElement: true,
+    showOffsets: true,
+    container: '#gjs',
+    height: '900px',
+    width: '100%',
+    selectorManager: { componentFirst: true },
+    plugins: [
+      presetWebpage,
+      basic,
+      gjsForms,
+      pluginCountdown,
+      tabs,
+      customCodePlugin,
+      parserPostCSS,
+      pluginTooltip,
+      imageEditor,
+      styleBg,
+    ],
+    pluginsOpts: {
+      [basic]: { flexGrid: true },
+      [presetWebpage]: {
+        modalImportTitle: 'Import Template',
+        modalImportLabel: '<div style="margin-bottom: 10px; font-size: 13px;">Paste here your HTML/CSS and click Import</div>',
+        modalImportContent(editor: { getHtml: () => any, getCss: () => any }) {
+          return `${editor.getHtml()}<style>${editor.getCss()}</style>`
+        },
+      },
+    },
     i18n: {
-      locale: "zh",
+      locale: 'zh',
       messages: { zh },
     },
 
-    projectData: JSON.parse(state.form.rawData || "{}"),
+    projectData: JSON.parse(state.form.rawData || '{}'),
 
     storageManager: false,
 
-    deviceManager: {
-      devices: [
-        { id: "desktop", name: "Desktop", width: "" },
-        {
-          id: "tablet",
-          name: "Tablet",
-          width: "768px",
-          widthMedia: "992px",
-        },
-        {
-          id: "mobilePortrait",
-          name: "Mobile portrait",
-          width: "320px",
-          widthMedia: "575px",
-        },
-      ],
-    },
+    // deviceManager: {
+    //   devices: [
+    //     { id: 'desktop', name: 'Desktop', width: '' },
+    //     {
+    //       id: 'tablet',
+    //       name: 'Tablet',
+    //       width: '768px',
+    //       widthMedia: '992px',
+    //     },
+    //     {
+    //       id: 'mobilePortrait',
+    //       name: 'Mobile portrait',
+    //       width: '320px',
+    //       widthMedia: '575px',
+    //     },
+    //   ],
+    // },
 
-    //自定义上传图片
+    // 自定义上传图片
     assetManager: {
       uploadFile: async (e: any) => {
-        const file = e.target.files[0];
+        const file = e.target.files[0]
         if (file) {
           try {
             // 判断文件大小，单位是字节，2MB = 2 * 1024 * 1024 字节
-            const maxSize = 2 * 1024 * 1024; // 2mb
+            const maxSize = 2 * 1024 * 1024 // 2mb
             if (Number(file.size) > Number(maxSize)) {
               ElMessage.warning({
-                message: "文件大小不能超过 2mb",
+                message: '文件大小不能超过 2mb',
                 center: true,
-              });
-              return;
-            } else {
-              // 调用自定义接口上传图片
-              const formData = new FormData();
-              formData.append("file", file);
-              formData.append("id", state.form.id);
-              api.uploadHomePageTemplateImage(formData).then((res: any) => {
-                editorRef.value.AssetManager.add(res.data);
-              });
+              })
             }
-          } catch (error) {
-            console.error("Image upload failed", error);
+            else {
+              // 调用自定义接口上传图片
+              const formData = new FormData()
+              formData.append('file', file)
+              formData.append('id', state.form.id)
+              api.uploadHomePageTemplateImage(formData).then((res: any) => {
+                editorRef.value.AssetManager.add(res.data)
+              })
+            }
+          }
+          catch (error) {
+            console.error('Image upload failed', error)
           }
         }
       },
@@ -129,38 +146,39 @@ const onDialogOpened = async () => {
     //     }
     //   ],
     // }
-  });
+  })
 
   // 通过事件改变框架的文本内容为中文
-  editorRef.value.on("block:custom", (props: any) => {
-    updataText(props.blocks);
-  });
+  editorRef.value.on('block:custom', (props: any) => {
+    updataText(props.blocks)
+  })
 
   // 监听图片删除事件
-  editorRef.value.on("asset:remove", (asset: any) => {
+  editorRef.value.on('asset:remove', (asset: any) => {
     // 先从 Asset Manager 中移除图片
     const asset1 = editorRef.value.AssetManager.getAll().find(
-      (a: any) => a.get("src") === asset.id,
-    );
+      (a: any) => a.get('src') === asset.id,
+    )
 
     if (asset1) {
       // 从 Asset Manager 中删除图片
-      editorRef.value.AssetManager.remove(asset);
+      editorRef.value.AssetManager.remove(asset)
 
       // 然后从静态网页的服务器上删除图片
       // deleteImageFromServer(src);
-    } else {
+    }
+    else {
       // console.log("未找到图片:", src);
     }
-  });
+  })
   // // 批量添加自定义块
   customBlock.forEach((item: any) => {
-    editorRef.value.Blocks.add(item.id, item);
-  });
+    editorRef.value.Blocks.add(item.id, item)
+  })
   // 添加自定义js组件类型
   htmlJsList.forEach((item) => {
-    editorRef.value.Components.addType(item.id, item);
-  });
+    editorRef.value.Components.addType(item.id, item)
+  })
 
   // const script = function () {
   //   alert('Hi');
@@ -185,73 +203,55 @@ const onDialogOpened = async () => {
   //   attributes: { class: 'fa fa-text' },
   //   content: { type: 'comp-with-js' },
   // });
-};
+}
 
 // 提交
-const save = async () => {
-  if (state.title === "设计模板") {
-    const html = editorRef.value.getHtml();
-    const css = editorRef.value.getCss();
-    const rawData = JSON.stringify(editorRef.value.getProjectData());
-    state.form = { ...state.form, html, css, rawData };
-    const { status } = await api.edit(state.form);
-    status === 1 &&
-      ElMessage.success({
-        message: "编辑成功",
-        center: true,
-      });
-    emits("fetch-data");
-    close();
+async function save() {
+  if (state.title === '设计模板') {
+    const html = editorRef.value.getHtml()
+    const css = editorRef.value.getCss()
+    const rawData = JSON.stringify(editorRef.value.getProjectData())
+    state.form = { ...state.form, html, css, rawData }
+    const { status } = await api.edit(state.form)
+    status === 1
+    && ElMessage.success({
+      message: '编辑成功',
+      center: true,
+    })
+    emits('fetch-data')
+    close()
   }
-};
+}
 
 // 关闭弹框
-const close = () => {
+function close() {
   if (editorRef.value) {
-    editorRef.value.destroy();
-    editorRef.value = null;
+    editorRef.value.destroy()
+    editorRef.value = null
   }
-  state.form = {};
-  state.dialogFormVisible = false;
-};
+  state.form = {}
+  state.dialogFormVisible = false
+}
 
 // 暴露数据
 defineExpose({
   showEdit,
-});
+})
 </script>
 
-<style scoped>
-.gjs-layer-name {
-  height: 20px;
-}
-
-/* #gjs {
-  border: none;
-}
-
-
-:deep(.gjs-one-bg ){
-  background-color: #78366a;
-}
-
-:deep(.gjs-two-color) {
-  color: rgba(255, 255, 255, 0.7);
-}
-
-:deep(.gjs-three-bg) {
-  background-color: #ec5896;
-  color: white;
-}
-
-:deep(.gjs-four-color,
-.gjs-four-color-h:hover) {
-  color: #ec5896;
-}
-:deep(:root){
-  --gjs-primary-color: #78366a;
-  --gjs-secondary-color: rgba(255, 255, 255, 0.7);
-  --gjs-tertiary-color: #ec5896;
-  --gjs-quaternary-color: #ec5896;
-} */
-</style>
+<template>
+  <el-drawer
+    v-if="state.dialogFormVisible" v-model="state.dialogFormVisible" :title="state.title" size="100%"
+    @opened="onDialogOpened" @close="close"
+  >
+    <div id="gjs" ref="formRef" />
+    <template #footer>
+      <el-button @click="close">
+        取 消
+      </el-button>
+      <el-button v-if="state.title === '设计模板'" type="primary" @click="save">
+        确 定
+      </el-button>
+    </template>
+  </el-drawer>
+</template>
